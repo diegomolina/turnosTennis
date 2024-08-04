96886c98e63818aced4866277441e770a7401215
Diego Molina <goa_molina@hotmail.com>
2024-08-03 19:53:36 -0500

    first commit

---
+++ turnos.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+CEDULA=$(cat config.json | jq .cedula --raw-output)
+LOL=$(cat config.json | jq .lol --raw-output)
+
+LOGIN=$(curl --path-as-is -s -k -X 'POST' \
+    -H 'Host: www.colsubsidio.com' -H 'Content-Type: application/json' \
+    --data "{\"documentNumber\":\"$CEDULA\",\"documentType\":2,\"password\":\"$LOL\"}" \
+    'https://www.colsubsidio.com/api-ssoc/users/login')
+
+#echo $LOGIN
+TOKEN=$(echo -n $LOGIN | jq .data.tokenId --raw-output)
+#echo "TOKEN: $TOKEN"
+
+SSO_FILE="cookie.txt"
+SSO=$(curl --path-as-is -i -s -k -c $SSO_FILE -X $'GET' --get \
+    -H $'Host: www.diversioncolsubsidio.com' \
+    -H $'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0' \
+    -H $'Accept: text/html' \
+    -b $'sistema=whatever' \
+    --data-urlencode "redireccion=https://www.diversioncolsubsidio.com//" \
+    --data-urlencode "autenticador=SesionPersonaColsubsidio" \
+    --data-urlencode "iPlanetDirectoryPro=$TOKEN" \
+    "https://www.diversioncolsubsidio.com/sistema.php/default/loguearSitio")
+
+#echo $SSO
+COOKIE=$(cat $SSO_FILE | tail -1 | egrep -o "([0-9a-z]+)$")
+rm $SSO_FILE 2> /dev/null
+
+#echo "Cookie: $COOKIE"
+
+# https://unix.stackexchange.com/questions/222368/get-date-of-next-saturday-from-a-given-date
+PREV_SAT=$(date -d "+ $(( ( (6 - 1 - $(date +%u)) % 7) + 1 )) days" +%Y-%m-%d)
+#PREV_MON=$(date -d "+ $(( ( (6 + 1 - $(date +%u)) % 7) + 1 )) days" +%Y-%m-%d)
+#NEXT_SAT=$(date -d "+ $(( ( (6 + 6 - $(date +%u)) % 7) + 1 )) days" +%Y-%m-%d)
+NEXT_MON=$(date -d "+ $(( ( (6 + 8 - $(date +%u)) % 7) + 1 )) days" +%Y-%m-%d)
+
+SCHEDULES=$(curl --path-as-is -s -k -X $'POST' \
+    -H $'Host: www.diversioncolsubsidio.com' \
+    -H $'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0' \
+    -H $'Accept: application/json, text/plain, */*' \
+    -H $'Content-Type: application/json;charset=utf-8' \
+    -b "sitio=$COOKIE" \
+    -d "{\"filtro_disponibilidad\":{\"fecha_inicio\":\"${PREV_SAT}T00:00:00-05:00\",\"fecha_fin\":\"${NEXT_MON}T23:59:59-05:00\"}}" \
+    "https://www.diversioncolsubsidio.com/v1/centro_entrenamiento/263/practicalibre/disponibilidad?filtrarSinCupo=0")
+
+#echo $SCHEDULES
+FILTER='.horarios[] | "\(.horario.fecha), \(.horario.hora_inicio), \(.cupos)"'
+JQ_SCHEDULES=$(echo $SCHEDULES | jq -r "$FILTER")
+
+#echo "$JQ_NEXT_SCHEDULES"
+
+while IFS= read -r line; do
+    IFS=", " read -r day hour turns <<< "$line"
+    echo "$(date -d $day +'%A %b %d'): $hour $turns"
+done <<< "$JQ_SCHEDULES"
